79a807184f2e6ca63e285de74579e957d26e4cbc
1234fed <145821930+1234fed@users.noreply.github.com>
2025-10-13 14:25:16 +0100

    Add files via upload

---
+++ 01 Drawing/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(194,254,150);
+  circle (200, 200, 300);
+  rect (110, 120, 50, 50);
+  rect (240, 120, 50, 50);
+  triangle (200, 310, 175, 280, 225, 280);
+  triangle (200, 310, 175, 280, 225, 280);
+
+
+}
