dd3d2672dcd4c5e1ad9a945cd468abe8cd41f7c6
1234fed <l.p.h.lukas@gmail.com>
2025-10-20 14:52:23 +0100

    update

---
+++ OLD/01.drawing/sketch.js
@@ -0,0 +1,16 @@
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0, 0, 255);
+  fill(247, 215, 52);
+  circle (200, 200, 300);
+  rect (110, 120, 50, 50);
+  rect (240, 120, 50, 50);
+  triangle (200, 310, 175, 280, 225, 280);
+  triangle (200, 310, 175, 280, 225, 280);
+
+
+}
---
+++ OLD/03.multiples/sketch.js
@@ -0,0 +1,21 @@
+let rgb = [255, 0, 0];    
+
+function setuop() {
+    createCanvas(WindowWidth, WindowHeight);
+    }
+
+function draw() {
+    background(220);
+
+    for(let i=0; i<=100; i=i+3){
+        r = random(70)
+        circle (100+r, i, i);  
+    }
+}
+
+function mouseclicked() {
+    if(select == 0){
+        select = 1;
+    }else
+        select = 0;
+}
