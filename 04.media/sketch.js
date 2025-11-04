51307f37bba1e96638485b4881ce0b1f6b5257f6
1234fed <l.p.h.lukas@gmail.com>
2025-11-04 11:34:17 +0000

    update

--- 04.media/sketch.js
+++ 04.media/sketch.js
@@ -1,30 +1,41 @@
 let img;
-let pixelation = 10;
+let pixelation = 15;
+let clearRadius = 50;
 
 function preload() {
-  img = loadImage("GOLD SQDN.jpg"); 
-  //https://drive.google.com/drive/u/0/home/
+  img = loadImage("GOLD-SQDN.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   pixelDensity(1);
+  noStroke();
 }
 
-function draw(){
-  image(img, 0, 0, width, height);
-  //pixelation_level = map(mouseX, 0, width, 0, 100); 
-  loadPixels();
-  noStroke();
+function draw() {
+  background(0);
+
+  // --- Draw pixelated image ---
+  img.loadPixels();
   for (let x = 0; x < width; x += pixelation) {
     for (let y = 0; y < height; y += pixelation) {
-      let i = (x + y * width) * 4;
-      let r = pixels[i + 0];
-      let g = pixels[i + 1];
-      let b = pixels[i + 2];
-      let a = pixels[i + 3];
-      fill(r, g, b, a);
+      // Map canvas coords to image coords so they align
+      let imgX = int(map(x, 0, width, 0, img.width));
+      let imgY = int(map(y, 0, height, 0, img.height));
+      let i = (imgX + imgY * img.width) * 4;
+      let r = img.pixels[i];
+      let g = img.pixels[i + 1];
+      let b = img.pixels[i + 2];
+      fill(r, g, b);
       square(x, y, pixelation);
     }
   }
+
+  // --- Draw clear area around cursor ---
+  drawingContext.save();
+  drawingContext.beginPath();
+  drawingContext.arc(mouseX, mouseY, clearRadius, 0, TWO_PI);
+  drawingContext.clip(); // only show inside circle
+  image(img, 0, 0, width, height); // full-size image, aligned
+  drawingContext.restore();
 }
